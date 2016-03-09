2cb76145a1893e0976b14a2ae89827d71dd7cac7
DraksterAU <drakster@me.com>
2016-03-09 23:12:31 +1100

    Another tooltip
    Another tooltip, for those that don't know

--- src/mod/gcewing/sg/SGIrisUpgradeItem.java
+++ src/mod/gcewing/sg/SGIrisUpgradeItem.java
@@ -11,11 +11,15 @@ import net.minecraft.item.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.util.*;
 import net.minecraft.world.*;
-
+import net.minecraft.util.EnumChatFormatting;
 import static gcewing.sg.BaseBlockUtils.*;
 
 public class SGIrisUpgradeItem extends BaseItem {
-
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+        list.add(EnumChatFormatting.GOLD + "Install this in a stargate to provide a redstone or computer controlled");
+        list.add(EnumChatFormatting.GOLD + "impenetrable barrier capable of blocking incoming gate travel");
+    }
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
         BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
