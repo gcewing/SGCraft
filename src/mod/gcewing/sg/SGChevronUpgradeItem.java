7aef9666a0ba79d4568be82c61faadfb3ebee9d4
DraksterAU <drakster@me.com>
2016-03-09 23:18:12 +1100

    Added Tooltip to chevron upgrade
    Minor change to give a brief one liner on how to use the upgrade item

--- src/mod/gcewing/sg/SGChevronUpgradeItem.java
+++ src/mod/gcewing/sg/SGChevronUpgradeItem.java
@@ -11,11 +11,15 @@ import net.minecraft.item.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.util.*;
 import net.minecraft.world.*;
-
+import net.minecraft.util.EnumChatFormatting;
 import static gcewing.sg.BaseBlockUtils.*;
 
 public class SGChevronUpgradeItem extends BaseItem {
-
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+        list.add(EnumChatFormatting.GOLD + "Install this in a stargate to upgrade it from 7 to 9 chevrons");
+        list.add(EnumChatFormatting.GOLD + "This will allow a stargate to connect to stargates on other worlds");
+    }
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
         BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
