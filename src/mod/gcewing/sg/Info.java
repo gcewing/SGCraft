b0242fb758a89667938040e618e3b28633b3eb89
Gregory Ewing <greg.ewing@canterbury.ac.nz>
2016-12-06 21:16:39 +1300

    Version number update

--- src/mod/gcewing/sg/Info.java
+++ src/mod/gcewing/sg/Info.java
@@ -6,7 +6,7 @@ public class Info {
 	static final String modName = "SG Craft";
 	static final String modID = "sgcraft";
 	//static final String modPackage = "";
-	static final String versionNumber = "1.13.1";
+	static final String versionNumber = "1.13.2";
 	static final String versionBounds = "[1.13,1.14)";
 	static final String acceptedMinecraftVersions = "[1.10.2,1.10.2]";
 }
