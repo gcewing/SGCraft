383f69dc390f30172ff2af22ad1008bc965ee1ac
Gregory Ewing <greg.ewing@canterbury.ac.nz>
2016-12-07 18:02:33 +1300

    Mark ring TE dirty on unmerge

--- src/mod/gcewing/sg/SGRingBlock.java
+++ src/mod/gcewing/sg/SGRingBlock.java
@@ -156,7 +156,7 @@ public class SGRingBlock extends SGBlock<SGRingTE> {
         if (te.isMerged && te.basePos.equals(basePos)) {
             //System.out.printf("SGRingBlock.unmergeFrom: unmerging\n");
             te.isMerged = false;
-            te.markBlockForUpdate();
+            te.markBlockChanged();
         }
     }
     
